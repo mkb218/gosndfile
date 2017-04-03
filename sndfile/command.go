b879248abfd33b2ebace27aac83ef8b059281e60
Matt Kane <mkane@spotify.com>
2017-04-03 12:34:32 -0400

    instructions for older libsndfile, fixes #13

--- sndfile/command.go
+++ sndfile/command.go
@@ -347,27 +347,6 @@ func trim(in string) string {
 	return "shelzbut" // shut up compiler
 }
 
-func broadcastFromC(c *C.SF_BROADCAST_INFO) *BroadcastInfo {
-	bi := new(BroadcastInfo)
-	bi.Description = trim(C.GoStringN(&c.description[0], C.int(len(c.description[:]))))
-	bi.Originator = trim(C.GoStringN(&c.originator[0], C.int(len(c.originator[:]))))
-	bi.Originator_reference = trim(C.GoStringN(&c.originator_reference[0], C.int(len(c.originator_reference[:]))))
-	bi.Origination_date = trim(C.GoStringN(&c.origination_date[0], C.int(len(c.origination_date[:]))))
-	bi.Origination_time = trim(C.GoStringN(&c.origination_time[0], C.int(len(c.origination_time[:]))))
-	bi.Time_reference_low = uint32(c.time_reference_low)
-	bi.Time_reference_high = uint32(c.time_reference_high)
-	bi.Version = uint16(c.version)
-	bi.Umid = trim(C.GoStringN(&c.umid[0], C.int(len(c.umid[:]))))
-	coding_history_bytes := make([]byte, 0, c.coding_history_size)
-	for i, r := range c.coding_history {
-		if i >= int(c.coding_history_size) {
-			break
-		}
-		coding_history_bytes = append(coding_history_bytes, byte(r))
-	}
-	return bi
-}
-
 // Retrieve the Broadcast Extension Chunk from WAV (and related) files.
 func (f *File) GetBroadcastInfo() (bi *BroadcastInfo, ok bool) {
 	bic := new(C.SF_BROADCAST_INFO)
